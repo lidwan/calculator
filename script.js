80e068abf8cc92526cb0bd60e5444b09581ff8b4
lidwan <contact@loayidwan.com>
2024-07-31 16:52:00 +0300

    basic operations working in console

---
+++ script.js
@@ -0,0 +1,22 @@
+//core functions
+
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+function divide(x, y){
+    return x / y;
+}
+
+console.log(add(4, 3));
+console.log(subtract(4, 3));
+console.log(multiply(4, 3));
+console.log(divide(4, 3));
